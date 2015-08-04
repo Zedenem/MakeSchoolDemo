364c97a490f1e0f1f6445f6c7e563021e2649a58
Zouhair Mahieddine <zouhair.mahieddine@gmail.com>
2015-08-04 11:14:24 -0700

    Initial Commit

---
+++ MakeSchoolApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MakeSchoolApp
+//
+//  Created by Zouhair Mahieddine on 8/4/15.
+//  Copyright (c) 2015 Dailymotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
